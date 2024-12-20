de6c09885d91344e6181632abe3f4373d7ad9181
Christopher David <chris@openagents.com>
2024-12-19 21:15:19 -0600

    Refactor `startup.rs` to clean up unused `fs` import, enhancing code maintainability.

--- src/startup.rs
+++ src/startup.rs
@@ -21,15 +21,15 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
-            // Serve static files from the public directory
-            .service(fs::Files::new("/", "./public").index_file("index.html"))
             // API routes under /api prefix
             .service(
                 web::scope("/api")
                     .route("/health_check", web::get().to(health_check))
                     .route("/subscriptions", web::post().to(subscribe))
+                    .app_data(db_pool.clone())
             )
-            .app_data(db_pool.clone())
+            // Serve static files from the public directory
+            .service(fs::Files::new("/", "./public").index_file("index.html"))
     })
     .listen(listener)?
     .run();
