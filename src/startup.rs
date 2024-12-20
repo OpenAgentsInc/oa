5323c1b7cfdaea35e1ec91d707bf2a0a88562dbd
Christopher David <chris@openagents.com>
2024-12-19 21:12:34 -0600

    Refactor `startup.rs` to log server port on startup using `tracing` library.

--- src/startup.rs
+++ src/startup.rs
@@ -5,16 +5,25 @@ use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::info;
 use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
+    let port = listener.local_addr().unwrap().port();
+    
+    // Log the server URL
+    info!(
+        "Server started! Access the web interface at http://localhost:{}",
+        port
+    );
+
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             // Serve static files from the public directory
             .service(fs::Files::new("/", "./public").index_file("index.html"))
-            // API routes
+            // API routes under /api prefix
             .service(
                 web::scope("/api")
                     .route("/health_check", web::get().to(health_check))
