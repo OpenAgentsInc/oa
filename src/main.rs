bdbd2c90605e19f37d445053e5905d6bde266444
Christopher David <chris@openagents.com>
2024-12-19 19:51:24 -0600

    Update config to oa

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!!!");
+use oa::configuration::get_configuration;
+use oa::startup::run;
+use oa::telemetry::{get_subscriber, init_subscriber};
+use sqlx::postgres::PgPoolOptions;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("oa".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool =
+        PgPoolOptions::new().connect_lazy_with(configuration.database.connect_options());
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
