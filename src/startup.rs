d1aff9034f6f3e4e47072b5525dfcf762a679b00
Christopher David <chris@openagents.com>
2024-12-30 23:46:34 -0600

    Add `actix_files` for static file serving in `startup.rs`.

--- src/startup.rs
+++ src/startup.rs
@@ -1,4 +1,5 @@
 use crate::routes::{health_check, share_chat, subscribe};
+use actix_files as fs;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
@@ -12,6 +13,10 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
             .route("/api/subscriptions", web::post().to(subscribe))
             .route("/api/v1/chats/{chat_id}/share", web::post().to(share_chat))
             .app_data(db_pool.clone())
+            // Serve principles.html at /principles
+            .service(fs::Files::new("/principles", "./public").index_file("principles.html"))
+            // Serve static files from the public directory
+            .service(fs::Files::new("/", "./public").index_file("index.html"))
     })
     .listen(listener)?
     .run();
