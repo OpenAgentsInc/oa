4a67f35615230202b8ec4b2d65b169b55a5d353d
Christopher David <chris@openagents.com>
2024-12-31 10:25:32 -0600

    Create file src/repo/postgres/mod.rs

---
+++ src/repo/postgres/mod.rs
@@ -0,0 +1,85 @@
+pub mod account;
+pub mod event;
+pub mod invoice;
+pub mod query;
+pub mod verification;
+
+use crate::error::Result;
+use crate::repo::postgres_migration::run_migrations;
+use crate::server::NostrMetrics;
+use async_trait::async_trait;
+use std::time::{Duration, Instant};
+use tracing::{info, warn};
+
+pub type PostgresPool = sqlx::pool::Pool<sqlx::Postgres>;
+
+pub struct PostgresRepo {
+    pub(crate) conn: PostgresPool,
+    pub(crate) conn_write: PostgresPool,
+    pub(crate) metrics: NostrMetrics,
+}
+
+impl PostgresRepo {
+    pub fn new(c: PostgresPool, cw: PostgresPool, m: NostrMetrics) -> PostgresRepo {
+        PostgresRepo {
+            conn: c,
+            conn_write: cw,
+            metrics: m,
+        }
+    }
+}
+
+/// Cleanup expired events on a regular basis
+async fn cleanup_expired(conn: PostgresPool, frequency: Duration) -> Result<()> {
+    tokio::task::spawn(async move {
+        loop {
+            tokio::select! {
+                _ = tokio::time::sleep(frequency) => {
+                    let start = Instant::now();
+                    let exp_res = delete_expired(conn.clone()).await;
+                    match exp_res {
+                        Ok(exp_count) => {
+                            if exp_count > 0 {
+                                info!("removed {} expired events in: {:?}", exp_count, start.elapsed());
+                            }
+                        },
+                        Err(e) => {
+                            warn!("could not remove expired events due to error: {:?}", e);
+                        }
+                    }
+                }
+            };
+        }
+    });
+    Ok(())
+}
+
+/// One-time deletion of all expired events
+async fn delete_expired(conn: PostgresPool) -> Result<u64> {
+    let mut tx = conn.begin().await?;
+    let update_count = sqlx::query("DELETE FROM \"event\" WHERE expires_at <= $1;")
+        .bind(chrono::Utc.timestamp_opt(crate::utils::unix_time() as i64, 0).unwrap())
+        .execute(&mut *tx)
+        .await?
+        .rows_affected();
+    tx.commit().await?;
+    Ok(update_count)
+}
+
+#[async_trait]
+impl crate::repo::NostrRepo for PostgresRepo {
+    async fn start(&self) -> Result<()> {
+        // begin a cleanup task for expired events.
+        cleanup_expired(self.conn_write.clone(), Duration::from_secs(600)).await?;
+        Ok(())
+    }
+
+    async fn migrate_up(&self) -> Result<usize> {
+        Ok(run_migrations(&self.conn_write).await?)
+    }
+
+    async fn optimize_db(&self) -> Result<()> {
+        // Not implemented for Postgres
+        Ok(())
+    }
+}
