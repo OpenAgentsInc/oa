f0b5c33034e584868f619f69e24e6dba3f405330
Christopher David <chris@openagents.com>
2024-12-31 10:00:18 -0600

    pull the rest in

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,20 @@ pub mod routes;
 pub mod startup;
 pub mod telemetry;
 
+pub mod cli;
 pub mod close;
 pub mod config;
 pub mod conn;
+pub mod db;
+pub mod delegation;
 pub mod error;
 pub mod event;
+pub mod info;
+pub mod nauthz;
+pub mod nip05;
+pub mod notice;
+pub mod payment;
+pub mod repo;
 pub mod server;
 pub mod subscription;
 pub mod utils;
-
-// Modules to be implemented later:
-// pub mod cli;
-// pub mod db;
-// pub mod delegation;
-// pub mod info;
-// pub mod nauthz;
-// pub mod nip05;
-// pub mod notice;
-// pub mod repo;
-// pub mod payment;
