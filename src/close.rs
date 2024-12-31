4f55560a712c38321ebd8665dc1e78d2cbab2d1b
Christopher David <chris@openagents.com>
2024-12-31 09:54:16 -0600

    Refactor `close.rs` to add serialization support and detailed struct for handling `CLOSE` commands, enhancing protocol command handling and subscription management.

--- src/close.rs
+++ src/close.rs
@@ -1,6 +1,32 @@
-use serde::Deserialize;
+//! Subscription close request parsing
+//!
+//! Representation and parsing of `CLOSE` messages sent from clients.
+use crate::error::{Error, Result};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+/// Close command in network format
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub struct CloseCmd {
+    /// Protocol command, expected to always be "CLOSE".
+    cmd: String,
+    /// The subscription identifier being closed.
+    id: String,
+}
+
+/// Identifier of the subscription to be closed.
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
 pub struct Close {
+    /// The subscription identifier being closed.
     pub id: String,
+}
+
+impl From<CloseCmd> for Result<Close> {
+    fn from(cc: CloseCmd) -> Result<Close> {
+        // ensure command is correct
+        if cc.cmd == "CLOSE" {
+            Ok(Close { id: cc.id })
+        } else {
+            Err(Error::CommandUnknownError)
+        }
+    }
 }
